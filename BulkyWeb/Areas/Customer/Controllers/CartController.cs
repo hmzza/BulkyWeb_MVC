e07bd8ce09c841bddc6629ab884695f39b82bb2e
hmzza <hamza.cstn@gmail.com>
2024-10-24 14:53:32 +0500

    Post Deployment - made url dynamic

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -127,7 +127,7 @@ namespace BulkyWeb.Areas.Customer.Controllers
             {
                 //it is a regular customer account and we need to capture payment
                 //stripe logic
-                var domain = "https://localhost:44334/";
+                var domain = Request.Scheme+"://"+Request.Host.Value+"/";
                 var options = new Stripe.Checkout.SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
